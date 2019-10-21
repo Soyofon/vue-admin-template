464ede634006d0771d5455d0af2f651695038a6d
Tonny Wang <tonny.wang@thinkpower-info.com.tw>
2019-10-21 12:31:25 +0800

    修改路由配置

---
+++ src/router/hiddenRoutes.js
@@ -0,0 +1,19 @@
+/**
+ *  不會顯示在目錄的路由放在這個檔案
+ */
+export const hiddenRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
